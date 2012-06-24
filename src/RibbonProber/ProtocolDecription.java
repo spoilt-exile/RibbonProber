5099276d381c50df4094d87233f19e98224fa454
Stanislav Nepochatov <spoilt.exile@gmail.com>
2012-06-24 17:59:34 +0300

    RibbonProber a1

---
+++ src/RibbonProber/ProtocolDecription.java
@@ -0,0 +1,202 @@
+/*
+ * This code is distributed under terms of GNU GPLv2.
+ * *See LICENSE file.
+ * ©UKRINFORM 2011-2012
+ */
+
+package RibbonProber;
+
+/**
+ * Class of protocol commands description and common tools
+ * @author Stanislav Nepochatov
+ */
+public abstract class ProtocolDecription {
+    
+    /**
+     * Commands array
+     */
+    private static java.util.ArrayList<ProtocolCommand> commands = new java.util.ArrayList<>();
+    
+    /**
+     * Command class
+     */
+    private static class ProtocolCommand {
+        
+        /**
+         * Name of command;
+         */
+        public String NAME;
+        
+        /**
+         * Doc string of command;
+         */
+        public String DOC;
+        
+        /**
+         * Auto paste string
+         */
+        public String AUTO_PASTE;
+        
+        /**
+         * Protocol level of command
+         */
+        public Integer COMMAND_LEVEL;
+        
+        /**
+         * Constructor for command
+         * @param givenName name of command
+         * @param givenDoc doc for command
+         * @param givenPaste paste arguments for command
+         */
+        ProtocolCommand(String givenName, String givenDoc, String givenPaste, Integer givenLevel) {
+            NAME = givenName;
+            DOC = givenDoc;
+            AUTO_PASTE = givenPaste;
+            COMMAND_LEVEL = givenLevel;
+        }
+    }
+    
+    /**
+     * Init protocol commands
+     */
+    public static void initProtocol() {
+        
+        commands.add(new ProtocolCommand("RIBBON_GREETINGS",
+                "[LEVEL_0] RIBBON_GREETINGS\n"
+                    + "Команда привітання з сервером.\n"
+                    + "Аргументи: ТИП_З’ЄДНАННЯ,ВЕРСІЯ_ПРОТОКОЛУ\n"
+                    + "Приклад: RIBBON_GREETINGS:CLIENT,a1",
+                ":CLIENT,a1", 0));
+        
+        commands.add(new ProtocolCommand("RIBBON_EXIT",
+                "RIBBON_EXIT:\n"
+                    + "Команда закриття з’єднання з сервером.\n"
+                    + "Аргументи: немає",
+                ":", 0));
+        
+        commands.add(new ProtocolCommand("RIBBON_LOGIN",
+                "[LEVEL_1] RIBBON_LOGIN\n" 
+                    + "Команда реєстрації користувача у системі.\n"
+                    + "Аргументи: ЛОГІН,ПАРОЛЬ у формі csv\n"
+                    + "Приклад: RIBBON_LOGIN:{USER},{PASSWORD}",
+                ":{guest},{guest}", 1));
+        
+        commands.add(new ProtocolCommand("RIBBON_GET_DIRS",
+                "[LEVEL_1] RIBBON_GET_DIRS"
+                + "Команда повертання списку напрямків системи у csv формі.\n"
+                + "Аргументы: немає",
+                ":", 1));
+        
+        commands.add(new ProtocolCommand("RIBBON_GET_TAGS",
+                "[LEVEL_1] RIBBON_GET_TAGS\n"
+                    + "Команда повертення списку тегів системи у csv формі.\n"
+                    + "Аргументи: немає",
+                ":", 1));
+        
+        commands.add(new ProtocolCommand("RIBBON_LOAD_BASE_FROM_INDEX",
+                "[LEVEL_1] RIBBON_LOAD_BASE_FROM_INDEX\n"
+                    + "Команда повертання списку індекса бази повідомлень.\n"
+                    + "починаючи з певного індексу.\n"
+                    + "Аргументи:ИНДЕКС або 0 (повернення усього індексу бази)\n"
+                    + "Приклад: RIBBON_LOAD_BASE_FROM_INDEX:0000000107",
+                ":0", 1));
+        
+        commands.add(new ProtocolCommand("RIBBON_POST_MESSAGE",
+                "[LEVEL_1] RIBBON_POST_MESSAGE\n"
+                    + "Команда випуску повідомлення до системи.\n"
+                    + "Аргументи:[НАПРЯМОК1,НАПРЯМОК2],{Заголовок},[ТЕГ1,ТЕГ2]\n"
+                    + "ТЕКСТ_ПОВІДОМЛЕННЯ\n"
+                    + "END:\n"
+                    + "Приклад: RIBBON_POST_MESSAGE:[СИСТЕМА.ТЕСТ],{Тестове повідомлення},[система,тест]\n"
+                    + "Це тестове повідомлення.\n"
+                    + "END:\n\n"
+                    + "Примітка: команда END: на кінці повідомлення обов’язкова,\n"
+                    + "без цієї команди сервер буде сприймати усі наступні команди\n"
+                    + "як текст повідомлення!",
+                ":[],{},[]\n\nEND:", 1));
+        
+        commands.add(new ProtocolCommand("RIBBON_GET_MESSAGE",
+                "[LEVEL_1] RIBBON_GET_MESSAGE\n"
+                    + "Команда одержання тексту повідомлення певного напрямку та індексу.\n"
+                    + "Аргументи:НАПРЯМОК,ІНДЕКС\n"
+                    + "Приклад: RIBBON_GET_MESSAGE:СИСТЕМА.ТЕСТ,0000000107",
+                ":", 1));
+        
+        commands.add(new ProtocolCommand("RIBBON_MODIFY_MESSAGE",
+                "[LEVEL_1] RIBBON_MODIFY_MESSAGE\n"
+                    + "Команда редагування тексту повідомлення певного напрямку за індексом.\n"
+                    + "Аргументи: НАПРЯМОК,ІНДЕКС\n"
+                    + "НОВИЙ_ТЕКСТ_ПОВІДОМЛЕННЯ\n"
+                    + "END:\n"
+                    + "Приклад:RIBBON_MODIFY_MESSAGE:СИСТЕМА.ТЕСТ,0000000107\n"
+                    + "Новий текст тестового повідомлення.\n"
+                    + "END:\n\n"
+                    + "Примітка: команда END: на кінці повідомлення обов’язкова,\n"
+                    + "без цієї команди сервер буде сприймати усі наступні команди\n"
+                    + "як текст повідомлення!",
+                ":\n\nEND:", 1));
+        
+        commands.add(new ProtocolCommand("RIBBON_DELETE_MESSAGE",
+                "[LEVEL_1] RIBBON_DELETE_MESSAGE\n"
+                + "Команда видаленя повідомлення з усіх напрямків за індексом.\n"
+                + "Аргументи:ІНДЕКС_ПОВІДОМЛЕННЯ\n"
+                + "Приклад:RIBBON_DELETE_MESSAGE:0000000107",
+                ":", 1
+                ));
+        
+        commands.add(new ProtocolCommand("RIBBON_SCTL_CONTROL_LOGIN",
+                "[LEVEL_2] RIBBON_SCTL_CONTROL_LOGIN\n"
+                + "Команда реєстрації адміністратора у системі.\n"
+                + "Аргументи: ЛОГІН,ПАРОЛЬ у формі csv\n"
+                + "Приклад: RIBBON_SCTL_CONTROL_LOGIN:{USER},{PASSWORD}",
+                ":{guest},{guest}", 2));
+    }
+    
+    /**
+     * Get list of commands names
+     * @return string array with names of RibbonProtocol commands
+     */
+    public static String[] getCommandList(Integer givenLevel) {
+        java.util.ListIterator<ProtocolCommand> commIter = commands.listIterator();
+        java.util.ArrayList<String> commList = new java.util.ArrayList<>();
+        while (commIter.hasNext()) {
+            ProtocolCommand currCommand = commIter.next();
+            if (currCommand.COMMAND_LEVEL == givenLevel || givenLevel == -1) {
+                commList.add(currCommand.NAME);
+            }
+        }
+        return commList.toArray(new String[commList.size()]);
+    }
+    
+    /**
+     * Get doc string of command
+     * @param givenName name to search
+     * @return doc of command
+     */
+    public static String getDoc(String givenName) {
+        java.util.ListIterator<ProtocolCommand> commIter = commands.listIterator();
+        while (commIter.hasNext()) {
+            ProtocolCommand currCommand = commIter.next();
+            if (currCommand.NAME.equals(givenName)) {
+                return currCommand.DOC;
+            }
+        }
+        return "";
+    }
+    
+    /**
+     * Get paste string of command
+     * @param givenName name to search
+     * @return paste string for command arguments
+     */
+    public static String getPaste(String givenName) {
+        java.util.ListIterator<ProtocolCommand> commIter = commands.listIterator();
+        while (commIter.hasNext()) {
+            ProtocolCommand currCommand = commIter.next();
+            if (currCommand.NAME.equals(givenName)) {
+                return currCommand.AUTO_PASTE;
+            }
+        }
+        return "";
+    }
+}
---
+++ src/RibbonProber/RibbonProber.java
@@ -0,0 +1,99 @@
+/*
+ * This code is distributed under terms of GNU GPLv2.
+ * *See LICENSE file.
+ * ©UKRINFORM 2011-2012
+ */
+
+package RibbonProber;
+
+/**
+ * Main class
+ * @author Stanislav Nepochatov
+ */
+public class RibbonProber {
+    
+    /**
+     * Client socket object
+     */
+    public static java.net.Socket ClientSocket;
+    
+    /**
+     * Stream of the answers from server
+     */
+    public static java.io.BufferedReader inStream;
+    
+    /**
+     * PrintWriter to the server
+     */
+    public static java.io.PrintWriter outStream;
+    
+    public static mainFrame mainWindow;
+    
+    private static proberThread networking;
+
+    /**
+     * @param args the command line arguments
+     */
+    public static void main(String[] args) {
+        ProtocolDecription.initProtocol();
+        mainWindow = new mainFrame();
+        mainWindow.setVisible(true);
+        mainWindow.addToLog("Дебагер завантажено та підготовлено до роботі.\n"
+                + "перед початком роботи з’єднайтись з сервером, вказавши адрес та порт сервера,\n"
+                + "та натиснувши кнопки 'З’єднати'\n"
+                + "для отримання справки оберіть команду протоколу, та натиснить кнопку '?'\n"
+                + "для формування команди, оберіть необхідну команду та натисніть кнопку 'Вставити'\n"
+                + "для запуску сформованної команди, натисніть кнопку 'Послати!'\n");
+    }
+    
+    /**
+     * Connect to server
+     */
+    public static void connect() {
+        if (networking != null) {
+            mainWindow.addToLog("ПОВІДОМЛЕННЯ: перепідключення\n");
+            outStream.println("RIBBON_EXIT:");
+        }
+        networking = new proberThread();
+        networking.start();
+    }
+    
+    /**
+     * Network communication thread
+     */
+    private static class proberThread extends Thread {
+        
+        proberThread() {
+            try {
+                RibbonProber.ClientSocket = new java.net.Socket(java.net.InetAddress.getByName(RibbonProber.mainWindow.ipField.getText()),
+                    Integer.parseInt(RibbonProber.mainWindow.portField.getText()));
+            } catch (java.io.IOException ex) {
+                mainWindow.addToLog("ПОМИЛКА: неможливо з'єднатися з сервером!\n");
+            }
+        }
+        
+        @Override
+        public void run() {
+            if (RibbonProber.ClientSocket != null) {
+                try {
+                    Boolean isAlive = true;
+                    inStream = new java.io.BufferedReader(new java.io.InputStreamReader(ClientSocket.getInputStream(), "UTF-8"));
+                    outStream = new java.io.PrintWriter(ClientSocket.getOutputStream(), true);
+                    mainWindow.addToLog("З'єднання успішно встановлено.\n");
+                    String inputLine;
+                    while (isAlive) {
+                       mainWindow.addToLog("< " + (inputLine = inStream.readLine()) + "\n");
+                       if (inputLine.equals("BYE:")) {
+                           isAlive = false;
+                       }
+                    }
+                } catch (java.lang.NullPointerException ex ) {
+                    mainWindow.addToLog("ПОМИЛКА: з'єднання розірвано!\n");
+                } catch (java.io.IOException ex) {
+                    mainWindow.addToLog("ПОМИЛКА: неможливо зчитати дані з сокету!\n");
+                }
+            }
+        }
+        
+    }
+}
